0a02d5b9613295bd374a5bac96df4f046cedd679
AbdulkadirKaraca <kadirkaraca1993@gmail.com>
2019-03-23 16:10:52 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace GezginAliciProblem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Harita = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Harita)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Harita
+            // 
+            this.Harita.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Harita.Location = new System.Drawing.Point(1, 1);
+            this.Harita.Name = "Harita";
+            this.Harita.Size = new System.Drawing.Size(600, 360);
+            this.Harita.TabIndex = 0;
+            this.Harita.TabStop = false;
+            this.Harita.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Harita_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(632, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(359, 316);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(479, 316);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(757, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(607, 140);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 5;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(745, 29);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(83, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Elle Yerleştir";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(745, 140);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(646, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 361);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Harita);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.Harita)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Harita;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tsp;
+using System.IO;
+using GezginAliciProblem.Properties;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace GezginAliciProblem
+{
+    public partial class Form1 : Form
+    {
+        public int mouseX, mouseY;
+        public int p;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* private void button1_Click(object sender, EventArgs e)
+         {
+             int x = checkBox1.Location.X;
+             int y = checkBox1.Location.Y;
+             int a = checkBox2.Location.X;
+             int b = checkBox2.Location.Y;
+             double uz1 = Math.Sqrt((x ^ 2 + y ^ 2));
+             double uz2 = Math.Sqrt((a ^ 2 + b ^ 2));
+             double sonuc = Math.Abs((uz1 - uz2));
+
+
+             label1.Text = sonuc.ToString();
+             double s = Math.Abs(x - a);
+             double s1 = Math.Abs(y - b);
+             double sonuc1 = Math.Sqrt(Math.Pow(s, 2) + Math.Pow(s1, 2));
+             s.ToString();
+             s1.ToString();
+
+             label2.Text = s + "   " + s1 + "  " + sonuc1;
+         }*/
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Location = new Point(250,50);
+            int A = this.Size.Height;
+            int B = this.Size.Width;
+
+            Harita.Height = this.Size.Height-40;
+            Harita.Width = this.Size.Width - 300;
+
+            
+           
+           
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            DataSet cityDS = new DataSet();
+            cityDS.ReadXml("C:\\Users\\Kadir\\Documents\\Visual Studio 2017\\Project\\GezginAliciProblem.UI\\GezginAliciProblem\\MarketList.xml"); // filename ile alınan xml dosyası okunur.
+            DataRowCollection cities = cityDS.Tables[0].Rows;
+            Rectangle recteski = new Rectangle(160,370,10,10);
+            foreach (DataRow city in cities)
+            {
+                listBox1.Items.Add(city["X"]);
+                listBox1.Items.Add(city["Y"]);
+                mouseX = Convert.ToInt16(city["X"]);
+                mouseY = Convert.ToInt16(city["Y"]);
+                Graphics g;
+                Pen whitePen = new Pen(Color.Blue, 2);
+                
+                Rectangle rect = new Rectangle(mouseX, mouseY, 10, 10);
+                g = Harita.CreateGraphics();
+                g.DrawEllipse(whitePen, rect);
+                
+
+                Harita.CreateGraphics();
+                
+                g.DrawLine(Pens.DarkRed, rect.Location,recteski.Location);
+
+                int x1 = rect.X;
+                int y1 = rect.Y;
+                int x2 = recteski.X;
+                int y2 = recteski.Y;
+                double s = (x2 - x1);
+                double s1 = (y2 - y1);
+                double sonuc1 = Math.Sqrt(Math.Pow(s, 2) + Math.Pow(s1, 2));
+                listBox2.Items.Add(sonuc1);
+                recteski = rect;
+
+            }
+
+            
+
+        }
+
+    
+
+        public void OpenCityList(string fileName) // XML dosyasını açma metodu
+        {
+            DataSet cityDS = new DataSet(); // dataset nesnemizi oluşturuyoruz.
+            try
+            {   
+               
+                cityDS.ReadXml(fileName); // filename ile alınan xml dosyası okunur.
+                DataRowCollection MarketList = cityDS.Tables[0].Rows;
+                foreach (DataRow city in MarketList)
+                {
+                    MessageBox.Show(city.ToString());
+
+                    //this.Add(new City(Convert.ToInt32(city["X"], CultureInfo.CurrentCulture), Convert.ToInt32(city["Y"], CultureInfo.CurrentCulture)));
+                } //datarowcollections nesnesi ile picture nesnesi içerisine şehirler yerleştirilir.
+            }
+            finally
+            {
+                cityDS.Dispose();
+            }
+        }
+            private void Harita_MouseClick(object sender, MouseEventArgs e)
+        {
+           
+            if (checkBox3.Checked == true)
+            {
+                mouseX = e.X;
+                mouseY = e.Y;
+                Graphics g;
+                Pen whitePen = new Pen(Color.Blue, 2);
+
+                Rectangle rect = new Rectangle(mouseX, mouseY, 10, 10);
+                g = Harita.CreateGraphics();
+                g.DrawEllipse(whitePen, rect);
+
+                Harita.CreateGraphics();
+
+                DosyaYaz(mouseX, mouseY);
+               g.DrawLine(Pens.DarkBlue, checkBox1.Location, checkBox2.Location);
+            }
+            //else MessageBox.Show("checbox'a  tıkla");
+           
+            
+
+            Point point = this.PointToClient(Cursor.Position);
+            if (point.X >= 100 && point.X <= 110 && point.Y >= 100 && point.Y <= 110)
+            {
+            
+            MessageBox.Show(point.ToString()+"  market 1");
+            }
+            //p++;//dire saymak için
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            /*System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+            System.Drawing.Graphics formGraphics;
+            formGraphics = this.CreateGraphics();
+            formGraphics.FillRectangle(myBrush, new Rectangle(e.X, e.Y, 200, 300));
+            myBrush.Dispose();
+            formGraphics.Dispose();*/
+            /* CheckBox c = new CheckBox();
+
+             c.Location = new Point(250,250);
+             c.Location = new Point(e.X, e.Y);
+             Controls.Add(c);*/
+            //MessageBox.Show(p.ToString());   //daire saymak için
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //CheckBox c = new CheckBox();
+            Graphics g;
+            Pen whitePen = new Pen(Color.Blue, 2);
+
+            Rectangle rect = new Rectangle(100, 100, 10, 10);
+            g = Harita.CreateGraphics();
+            //c.Location = new Point(100,100);
+            //Harita.Controls.Add(c);
+            
+            g.DrawEllipse(whitePen, rect);
+
+
+            Harita.CreateGraphics();
+        }
+
+        private static void DosyaYaz(int a,int b) {
+            StreamWriter sw = File.AppendText("C:\\Users\\Kadir\\Desktop\\metinbelgesi1.txt");
+            sw.Write(a + ";" + b+",");
+            sw.Close();
+            
+            
+            /*String Yol= @"C:\Users\Kadir\Desktop\metinbelgesi.txt";
+            FileStream fs = new FileStream(Yol, FileMode.Open, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            //Yazma işlemi için bir StreamWriter nesnesi oluşturduk.
+            sw.WriteLine(a+";"+b);
+           // sw.WriteLine(b);
+            //Dosyaya ekleyeceğimiz iki satırlık yazıyı WriteLine() metodu ile yazacağız.
+            sw.Flush();
+            //Veriyi tampon bölgeden dosyaya aktardık.
+            sw.Close();
+            fs.Close();*/
+
+        }
+        
+    }
+}
